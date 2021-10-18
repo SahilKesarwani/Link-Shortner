8f13044cb3b4afb1e50eb26b808b3de2101e3436
Sahil Kesarwani <80140802+SahilKesarwani@users.noreply.github.com>
2021-10-18 17:45:24 +0530

    Initial setup

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+
+const App = () => {
+	return (
+		<div>
+			<Navbar />
+			<h1>Link Shortner</h1>
+			<h2>one stop destination for all your problems</h2>
+		</div>
+	);
+};
+
+export default App;
