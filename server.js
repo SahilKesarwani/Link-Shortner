b35363ce03c0b4b52f243369d2564cb323a9f92a
Sahil <sahilkesarwani2@gmail.com>
2023-02-23 14:48:36 +0530

    Remove Heroku

--- server.js
+++ server.js
@@ -25,8 +25,14 @@ mongoose
 app.use("/", require("./routes/index"));
 app.use("/api/link", require("./routes/link"));
 
+app.use(express.static(path.join(__dirname, "./client/build")));
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+/*
 // Serve static assets if in production
-if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === "production" || process.env.NODE_ENV === "staging") {
 	// Set static folder
 	app.use(express.static("client/build"));
 
@@ -34,6 +40,7 @@ if (process.env.NODE_ENV === "production") {
 		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 	});
 }
+*/
 
 app.listen(PORT, () => {
 	console.log(`Server is running at port ${PORT}`);
